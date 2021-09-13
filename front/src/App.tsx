eb9fca18c3a05bc9e2ab966a8364ea8dff27fa19
William Leite <william.a.antunesleite@gmail.com>
2021-09-13 04:01:55 -0300

    alteração no layout

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -14,17 +14,17 @@ function App() {
 
         <div className="row px-3">
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Taxa de Sucesso(%)</h5>
+            <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
             <BarChart />
           </div>
           <div className="col-sm-6">
-            <h5 className="text-center text-secondary">Todas as Vendas</h5>
+            <h5 className="text-center text-secondary">Todas as Vendas (%)</h5>
             <DonutChart />
           </div>
         </div>
 
         <div className="py-3">
-          <h2 className="text-primary">Todas as vendas</h2>          
+          <h2 className="text-primary">Tabela de vendas</h2>          
         </div>
 
         <DataTable />
