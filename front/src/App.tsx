c322a70f12a288d177c5452b6af571f6ae11c16a
William Leite <william.a.antunesleite@gmail.com>
2021-09-13 03:42:47 -0300

    correçoes feitas nos codigos do projeto

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,6 @@
 import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
@@ -17,13 +18,13 @@ function App() {
             <BarChart />
           </div>
           <div className="col-sm-6">
-            <h5 >Todas as Vendas</h5>
-            <BarChart />
+            <h5 className="text-center text-secondary">Todas as Vendas</h5>
+            <DonutChart />
           </div>
         </div>
 
         <div className="py-3">
-          <h2></h2>          
+          <h2 className="text-primary">Todas as vendas</h2>          
         </div>
 
         <DataTable />
