de9a27d6ba521bd63d4dc45ef4cdc86ce3379a3d
William Leite <william.a.antunesleite@gmail.com>
2021-09-18 04:39:22 -0300

    pagina web

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dsvendas.repositories;
 
-import java.util.List;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,7 +8,7 @@ import com.devsuperior.dsvendas.entities.Seller;
 
 public interface SellerRepository extends JpaRepository<Seller,Long>{
 
-	List<Seller> findAll();
+	
 
 }
  
